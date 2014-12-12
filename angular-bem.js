d7cfdc1d661956b49bd4aaf5b08d1248801a08b3
Andrey Yamanov <tenphi@gmail.com>
2014-12-12 23:46:54 +0300

    add debug mode

--- angular-bem.js
+++ angular-bem.js
@@ -4,6 +4,24 @@
     throw new Error('angular-bem: angular required');
   }
 
+  function hashCode(str) { // java String#hashCode
+    var hash = 0;
+    for (var i = 0; i < str.length; i++) {
+      hash = str.charCodeAt(i) + ((hash << 5) - hash);
+    }
+    return hash;
+  }
+
+  function intToRGB(i){
+    return ('00' + ((i>>16)&0xFF/1.5).toString(16)).slice(-2) +
+    ('00' + ((i>>8)&0xFF/1.5).toString(16)).slice(-2) +
+    ('00' + (i&0xFF/1.5).toString(16)).slice(-2);
+  }
+
+  function colorByString(str) {
+    return '#' + intToRGB(hashCode(str));
+  }
+
   // modName -> mod_name
   function formatName(s) {
     return s.replace(/[A-Z]/g, function(s) {return '-' + s.toLowerCase();});
@@ -20,6 +38,56 @@
     }
   }
 
+  var debugOuterStyle = {
+    minHeight: '9px',
+    outline: '1px solid #888',
+    paddingTop: '10px'
+  };
+
+  var debugInnerStyle = {
+    fontFamily: 'monospace',
+    position: 'absolute',
+    left: 0,
+    top: 0,
+    fontSize: '8px',
+    color: 'white',
+    outline: '1px solid #888'
+  };
+
+  function injectDebug($el, blockName, elementNames, mods) {
+    var pos = $el.css('position');
+    mods = mods || {};
+    mods = (function(mods) {
+      return Object.keys(mods)
+        .filter(function(mod) {return !!mods[mod];})
+        .map(function(mod) {
+          return mod + ':' + mods[mod];
+        }).join(',');
+    })(mods);
+    var className = 'ng-bem-debug-' + (elementNames ? 'element' : 'block');
+    var text = (elementNames ? '(' + elementNames.join(',') + ')' : '[' + blockName + ']')
+      + (mods ? '{' + mods + '}' : '');
+
+    var $debug = angular.element($el.children()[$el.children().length - 1]);
+    if (!$debug.length || !~getClasses($debug).indexOf(className)) {
+      $debug = angular.element('<div>').addClass(className);
+    }
+
+    $debug.text(text);
+
+    if (pos !== 'absolute' && pos !== 'relative') {
+      $el.css('position', 'relative');
+    }
+
+    $el.css(debugOuterStyle);
+
+    $debug.css(debugInnerStyle);
+
+    $debug.css('backgroundColor', colorByString(blockName));
+
+    return $debug;
+  }
+
   var classListSupport = !!document.createElement('div').classList, getClasses, addClass, removeClass;
 
   if (classListSupport) {
@@ -34,6 +102,7 @@
     removeClass = function(el, cls) {
       el[0].classList.remove(cls);
     };
+
   } else {
     getClasses = function(el) {
       return el[0].className.split(/\s/g);
@@ -53,7 +122,6 @@
     var size = prefix.length, cls, i, len;
     for (i = 0, len = classes.length; i < len; i++) {
       cls = classes[i];
-      console.log(cls, prefix);
       if (cls.slice(0, size) === prefix && except !== cls) {
         removeClass(el, cls);
       }
@@ -62,9 +130,29 @@
 
   var module = angular.module('tenphi.bem', []);
 
-  module.directive('block', function($compile) {
+  module.provider('bemConfig', function() {
+    var debug = false;
+
+    this.debug = function(bool) {
+      debug = !!bool;
+    };
+
+    this.debugInnerStyle = debugInnerStyle;
+
+    this.debugOuterStyle = debugOuterStyle;
+
+    this.$get = function() {
+      return {
+        debug: debug
+      }
+    };
+  });
+
+  module.directive('block', function(bemConfig) {
+    var debug = bemConfig.debug;
     return {
       restrict: 'EA',
+      require: 'block',
       controller: function BlockCtrl($scope, $element, $attrs) {
         this.$el = $element;
         var $tmp = $scope;
@@ -81,45 +169,68 @@
 
         var blockName = $attrs.block;
         this.name = blockName;
+        this.$el = $element;
 
         $element[0].removeAttribute('block');
         addClass($element, blockName);
+      },
+      link: {
+        pre: function(scope, $el, attrs, ctrl) {
+          if (debug) {
+            ctrl.debugElement = injectDebug($el, ctrl.name);
+          }
+        },
+        post: function(scope, $el, attrs, ctrl) {
+          if (debug) {
+            $el.append(ctrl.debugElement);
+          }
+        }
       }
     }
   });
 
-  module.directive('element', function() {
+  module.directive('element', function(bemConfig) {
+    var debug = bemConfig.debug;
     return {
       restrict: 'EA',
       require: ['^block', 'element'],
       controller: function ElementCtrl($element) {
         this.$el = $element;
       },
-      link: function(scope, el, attrs, ctrls) {
-        var blockCtrl = ctrls[0];
-        var elementCtrl = ctrls[1];
+      link: {
+        pre: function(scope, $el, attrs, ctrls) {
+          var blockCtrl = ctrls[0];
+          var elementCtrl = ctrls[1];
 
-        if (elementCtrl && blockCtrl.$el[0] === elementCtrl.$el[0]) {
-          blockCtrl = blockCtrl.parent;
-          if (!blockCtrl) {
-            throw new Error('angular-bem: element doesn\'t have parent block');
+          if (elementCtrl && blockCtrl.$el[0] === elementCtrl.$el[0]) {
+            blockCtrl = blockCtrl.parent;
+            if (!blockCtrl) {
+              throw new Error('angular-bem: element doesn\'t have parent block');
+            }
           }
+
+          elementCtrl.blockName = blockCtrl.name;
+          elementCtrl.names = [];
+
+          if (!attrs.element) return;
+
+          // handle multiple elements attached to one node
+          elementCtrl.names = attrs.element.split(/\s/g);
+
+          elementCtrl.names.forEach(function(name) {
+            removeClass($el, name);
+            addClass($el, blockCtrl.name + '__' + name);
+          });
+
+          $el[0].removeAttribute('element');
+
+          if (debug) {
+            this.debugElement = injectDebug($el, blockCtrl.name, elementCtrl.names);
+          }
+        },
+        post: function(scope, $el, attrs, ctrls) {
+          $el.append(this.debugElement);
         }
-
-        elementCtrl.blockName = blockCtrl.name;
-        elementCtrl.names = [];
-
-        if (!attrs.element) return;
-
-        // handle multiple elements attached to one node
-        elementCtrl.names = attrs.element.split(/\s/g);
-
-        elementCtrl.names.forEach(function(name) {
-          removeClass(el, name);
-          addClass(el, blockCtrl.name + '__' + name);
-        });
-
-        el[0].removeAttribute('element');
       }
     }
   });
@@ -168,11 +279,12 @@
     }
   });
 
-  module.directive('elementMod', function() {
+  module.directive('elementMod', function(bemConfig) {
+    var debug = bemConfig.debug;
     return {
       restrict: 'A',
       require: 'element',
-      link: function(scope, el, attrs, ctrl) {
+      link: function(scope, $el, attrs, ctrl) {
         var modMap = {};
 
         function setMod() {
@@ -181,7 +293,7 @@
 
           if (!modMap) {
             elementNames.forEach(function(elementName) {
-              removeClassesWithPrefix(el, blockName + '__' + elementName + '_');
+              removeClassesWithPrefix($el, blockName + '__' + elementName + '_');
             });
             return;
           }
@@ -201,11 +313,15 @@
               var className = classPrefix
                 + (typeof(modValue) === 'string' ? '_' + modValue : '');
 
-              removeClassesWithPrefix(el, classPrefix, modValue ? className : null);
+              removeClassesWithPrefix($el, classPrefix, modValue ? className : null);
               if (modValue) {
-                addClass(el, className);
+                addClass($el, className);
               }
             });
+          }
+
+          if (debug) {
+            this.debugElement = injectDebug($el, blockName, elementNames, modMap);
           }
         }
 
@@ -214,7 +330,7 @@
           setMod();
         }, true);
 
-        el[0].removeAttribute('element-mod');
+        $el[0].removeAttribute('element-mod');
       }
     }
   });
