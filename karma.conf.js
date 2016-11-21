5570682f8d12b9b4dd790bada95aa692f3c56ca0
Dave Staab <davestaab@gmail.com>
2016-11-21 13:47:58 -0500

    dropping IE and Chrome browsers from karma conf because travis doesn't support them.

--- karma.conf.js
+++ karma.conf.js
@@ -56,8 +56,8 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    // browsers: ['IE'],
-    browsers: ['PhantomJS', 'IE', 'Chrome'],
+    // browsers: ['PhantomJS', 'IE', 'Chrome'],
+    browsers: ['PhantomJS'],
 
 
     // Continuous Integration mode
